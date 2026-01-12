b3d876f1638c274dd5c3e0f0ec2cecb6c70587fd
Ivy <zosa.ia@obmontessori.edu.ph>
2026-01-12 11:38:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from pyscript import display, document, window
 def Convert_and_Check(e):
     document.getElementById('output').innerHTML = ' '
 
-    Celsius = float(document.getElementById('InputTemperature').value)
-    Farenheitunrounded = float(Celsius * (9/5) + 32)
-    Farenheit = round(Farenheitunrounded, 2)
+    Farenheit = float(document.getElementById('InputTemperature').value)
+    Celsiusunrounded = float((Farenheit - 32) * (5/9))
+    Celsius = round(Celsiusunrounded, 2)
 
-    if Farenheit >= 37.8:
-        display(f'Your temperature in Farenheit is {Farenheit}. Rest well, for your temperature indicates a fever.', target='output')
+    if Celsius >= 37.8:
+        display(f'Your temperature in Farenheit is {Celsius}. Rest well, for your temperature indicates a fever.', target='output')
     else:
-        display(f'Your temperature in Farenheit is {Farenheit}. Best maintain it, a healthy number isn\'t always common.', target='output')
+        display(f'Your temperature in Farenheit is {Celsius}. Best maintain it, a healthy number isn\'t always common.', target='output')
